18f273be0518bd9cf3baa5405481ea6aca0089e9
frederic <flausson@normandiewebschool.fr>
2024-06-23 17:20:08 +0200

    feat: ✨ auth api login and register

--- api-auth/src/controllers/auth.controller.js
+++ api-auth/src/controllers/auth.controller.js
@@ -1,5 +1,100 @@
+import { createClient } from '@supabase/supabase-js';
+
+//--------------------------------------------------
+
+const createAuthUser = async (email, password) => {
+  try {
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    );
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    if (error) throw error;
+
+    return data.user.id;
+  } catch (error) {
+    console.error(error.message);
+    throw error;
+  }
+};
+
+const createDbUser = async (userData) => {
+  try {
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    );
+    const { data, error } = await supabase.from('user').insert(userData);
+
+    if (error) throw error;
+
+    return data;
+  } catch (error) {
+    console.error(error.message);
+    throw error;
+  }
+};
+
+const authLogin = async (email, password) => {
+  try {
+    const supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+    );
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) throw error;
+
+    return data;
+  } catch (error) {
+    console.error(error.message);
+    throw error;
+  }
+};
+
+//--------------------------------------------------
+
 async function getAuth(req, reply, fastify) {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
   reply.send({ message: `Hello from api-auth` });
 }
 
-export default getAuth;
+async function postSignup(req, reply, fastify) {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+  let { email, password } = req.body;
+  try {
+    let authid = await createAuthUser(email, password);
+    await createDbUser({ auth_id: authid, username: email, email });
+    reply.send({ message: `Successfully signed up !` });
+  } catch (error) {
+    reply.status(400).send({ error: 'Failed to sign user up' });
+  }
+}
+
+async function postLogin(req, reply, fastify) {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+  let { email, password } = req.body;
+  try {
+    let data = await authLogin(email, password);
+    console.log(data);
+    reply.send(data);
+  } catch (error) {}
+  reply.status(400).send({ error: 'Failed to login' });
+}
+
+export { getAuth, postSignup, postLogin };
--- api-auth/src/routes/auth.routes.js
+++ api-auth/src/routes/auth.routes.js
@@ -1,4 +1,4 @@
-import getAuth from '../controllers/auth.controller.js';
+import { getAuth, postSignup, postLogin } from '../controllers/auth.controller.js';
 
 const authOpts = {
   schema: {
@@ -15,8 +15,40 @@ const authOpts = {
   handler: getAuth,
 };
 
+const signupOpts = {
+  schema: {
+    tags: ['Auth'],
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          message: { type: 'string' },
+        },
+      },
+    },
+  },
+  handler: postSignup,
+};
+
+const loginOpts = {
+  schema: {
+    tags: ['Auth'],
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          message: { type: 'string' },
+        },
+      },
+    },
+  },
+  handler: postLogin,
+};
+
 const authRoutes = async (fastify, options) => {
   fastify.get('/auth', authOpts);
+  fastify.post('/auth/signup', signupOpts);
+  fastify.post('/auth/login', loginOpts);
 };
 
 export default authRoutes;
