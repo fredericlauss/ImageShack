c9e84c81ac14d20f0280a716d946d31f33832eef
frederic <flausson@normandiewebschool.fr>
2024-08-09 11:35:33 +0200

    feat: ✨ service worker for offline

---
+++ next-front/app/offline/page.tsx
@@ -0,0 +1,11 @@
+export default function Offline() {
+  return (
+    <div>
+      <h1>Vous êtes hors ligne</h1>
+      <p>
+        Il semble que vous n'êtes pas connecté à Internet. Veuillez vérifier
+        votre connexion.
+      </p>
+    </div>
+  );
+}
